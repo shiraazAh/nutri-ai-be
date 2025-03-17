540438628759b651b2de80a71f6610a5168b991c
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2025-03-17 05:30:04 +0000

    Add: cors package

--- index.js
+++ index.js
@@ -2,19 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const OpenAI = require("openai");
 const axios = require("axios");
+const cors = require('cors')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use(cors())
 app.use(express.json());
-
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
